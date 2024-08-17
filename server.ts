eb99358d5c32f3d424ef261be91f4205c2baf721
imbhaskaran <imbhaskaran@gmail>
2024-08-17 22:24:57 +0530

    initial commit

---
+++ server.ts
@@ -0,0 +1,29 @@
+import { createServer } from "http";
+import app from "./src/app";
+import { Socket } from "socket.io";
+import { Server } from "socket.io";
+
+const PORT = process.env.PORT || 5000;
+
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket: Socket) => {
+  io.emit("message", "connected to server");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("message", (msg: string) => {
+    console.log(msg);
+    io.emit("message", "the server says: " + msg);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
