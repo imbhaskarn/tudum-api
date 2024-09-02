0075494065588295f8122d5c61ab145d589ac030
imbhaskaran <imbhaskaran@gmail>
2024-09-02 20:57:49 +0530

    room setup

--- server.ts
+++ server.ts
@@ -16,12 +16,7 @@ const io = new Server(server, {
 });
 
 io.on('connection', (socket: Socket) => {
-  const roomName = 'some_room';
-  socket.join(roomName);
-  console.log(`${socket.id} joined room: ${roomName}`);
-  console.log('a user connected');
-
-  socket.on('joinUserRoom', (userId) => {
+  socket.on('roomJoined', (userId: string) => {
     // Create or join a room bound to the user's ID
     const userRoom = `room_${userId}`;
     socket.join(userRoom);
@@ -29,17 +24,22 @@ io.on('connection', (socket: Socket) => {
     // Optionally send a confirmation back to the client
     socket.emit('roomJoined', userRoom);
   });
+  socket.on('leaveRoom', (roomId) => {
+    socket.leave(roomId);
+    console.log(`User ${socket.id} left room ${roomId}`);
+  });
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-  socket.on('message', (msg: string) => {
-    console.log(msg);
-    io.emit('message', 'the server says: ' + msg);
-  });
-  socket.on('message', (msg: string) => {
-    console.log(msg);
-    socket.broadcast.emit('message', { message: msg, sender: socket.id });
+
+  socket.on('message', ({ roomId, message }) => {
+    console.log({ roomId, message });
+    io.to(roomId).emit('message', {
+      content: message,
+      user: socket.id,
+      roomId
+    });
   });
 });
 
